a01b4859173936df7672b8619a380ee061ad6667
Aurélien Demilly <demilly.aurelien@gmail.com>
2017-04-07 18:04:03 +0200

    add "-d" inline option to display line count per file

--- lc.go
+++ lc.go
@@ -10,6 +10,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"path"
+	"strings"
 )
 
 // usage print usage string to standard output
@@ -45,6 +47,26 @@ func countLines(r io.Reader, acc chan int64) {
 	close(acc)
 }
 
+// handleArgs handles command line arguments
+// returns a slice of strings where
+// - head contains behavior tags
+// - tails is the list of files to count lines from
+func handleArgs(cmdArgs []string) []string {
+	args := []string{
+		"",
+	}
+
+	for _, in := range cmdArgs {
+		if in != "-d" {
+			args = append(args, in)
+		} else {
+			args[0] += "DETAILS|"
+		}
+	}
+
+	return args
+}
+
 // main checks command line arguments and compute line count from them
 func main() {
 
@@ -53,9 +75,10 @@ func main() {
 		return
 	}
 
+	args := handleArgs(os.Args[1:])
 	acc := make([]chan int64, 0)
 
-	for i, arg := range os.Args[1:] {
+	for i, arg := range args[1:] {
 		acc = append(acc, make(chan int64))
 		go countLines(source(arg), acc[i])
 	}
@@ -64,6 +87,10 @@ func main() {
 	for i := range acc {
 		for c := range acc[i] {
 			count += c
+
+			if strings.Contains(args[0], "DETAILS") {
+				fmt.Println(c, "\t", path.Base(args[1+i]))
+			}
 		}
 	}
 	fmt.Printf("%v\n", count)
